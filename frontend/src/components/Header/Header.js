ca48bd2d06c409e2d5213d0ec2104841d5a03813
codingkrills <pulkitsinghmail@gmail.com>
2021-11-22 22:34:46 +0530

    updates

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Header.css'
 
 const Header = () => {
@@ -7,6 +8,7 @@ const Header = () => {
             <a href="#default" class="logo">CompanyLogo</a>
             <div class="header-right">
                 <a class="active" href="#home">HELLO ADMIN</a>
+                <Link className="logout" to="/">LOG OUT</Link>
                 {/* <a href="#contact">Contact</a>
                 <a href="#about">About</a> */}
             </div>
