cb6f1879b617749872749b270f66599a6a90bb41
codingkrills <pulkitsinghmail@gmail.com>
2022-01-09 20:59:07 +0530

    updates

--- frontend/src/pages/Student/StudentAttendanceTable.js
+++ frontend/src/pages/Student/StudentAttendanceTable.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux'
 import StudentSideBar from '../../components/SideBar/StudentSideBar'
 import Header from '../../components/Header/Header'
 import ShowAllStudentAttendanceTable from '../../components/Tables/ShowAllStudentAttendanceTable'
 
 const StudentAttendanceTable = () => {
+
+    // * ======================== authentication
+    const navigate = useNavigate();
+
+    // * ==== get user state 
+    const adminLogin = useSelector((state) => state.adminLogin)
+    const { loading, error, userInfo, message } = adminLogin;
+
+    // alert(JSON.stringify(userInfo))
+
+    // * USE EFFECT REDIRECT TO LOG IN 
+    useEffect(() => {
+        if (!userInfo) {
+            navigate("/");
+        }
+        if (userInfo) {
+
+            if (userInfo.is_student === true) {
+
+            }
+            else {
+                navigate("/");
+            }
+        }
+        else {
+            navigate("/");
+        }
+    }, [navigate, userInfo])
+
+    // * ======================== authentication
+
     return (
         <div>
             <Header />
